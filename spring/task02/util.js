5b24204f30bdab82809907adb7fa0e904453df7e
rjwx60 <614054722@qq.com>
2019-02-18 23:55:22 +0800

    ..

--- spring/task02/util.js
+++ spring/task02/util.js
@@ -8,16 +8,16 @@
 //  2-1 实践判断各种数据类型的方法
 // 判断 arr 是否为一个数组，返回一个 boolean 值
 function isArray(arr) {
-    return Array.isArray && Array.isArray(arr) || Object.prototype.toString.call(arr) == "[object Array]";
+	return Array.isArray && Array.isArray(arr) || Object.prototype.toString.call(arr) == "[object Array]";
 }
 
 // 判断 fn 是否为一个函数，返回一个 boolean 值
 function isFunction(fn) {
-    return typeof(fn) == "function"
+	return typeof (fn) == "function"
 }
 
 // 判断综合
-function returnType(value){
+function returnType(value) {
 	return Object.prototype.toString.call(value).slice(8, -1).toLowerCase();
 }
 
@@ -28,7 +28,7 @@ function returnType(value){
 // 实现深拷贝，源对象限制为数字、字符串、布尔、日期、数组、Object对象
 function deepClone1(sourceObj) {
 	var targetType = Object.prototype.toString.call(sourceObj).slice(8, -1).toLowerCase(), cacheObj = null;
-	switch(targetType){
+	switch (targetType) {
 		case "number":
 		case "string":
 		case "boolean":
@@ -36,16 +36,16 @@ function deepClone1(sourceObj) {
 			break;
 		case "array":
 			cacheObj = [];
-			for(var i = 0; i < sourceObj.length; i++){
-				cacheObj[i] = deepClone1(sourceObj[i]);
+			for (var i = 0; i < sourceObj.length; i++) {
+				cacheObj[ i ] = deepClone1(sourceObj[ i ]);
 			}
 			break;
 		case "object":
 		case "date":
 			cacheObj = {};
-			for(var key in sourceObj){
-				if(sourceObj.hasOwnProperty(key)){
-					cacheObj[key] = deepClone1(sourceObj[key]);
+			for (var key in sourceObj) {
+				if (sourceObj.hasOwnProperty(key)) {
+					cacheObj[ key ] = deepClone1(sourceObj[ key ]);
 				}
 			}
 			break;
@@ -55,61 +55,61 @@ function deepClone1(sourceObj) {
 	return cacheObj;
 }
 
-var deepClone2 = function(obj) { 
-    var cacheValue;   
-    if (Object.prototype.toString.call(obj) === "[object Array]") { 
-        cacheValue = [];  
-        while (obj.length--) { 
-            cacheValue[i] = deepClone2(obj[i]); 
-        }
-        return cacheValue;
-    } else if (Object.prototype.toString.call(obj) === "[object Object]"){ 
-        cacheValue = {}; 
-        for (var k in obj) { 
-			if(obj.hasOwnProperty(k)){
-				cacheValue[k] = deepClone2(obj[k]); 
+var deepClone2 = function (obj) {
+	var cacheValue;
+	if (Object.prototype.toString.call(obj) === "[object Array]") {
+		cacheValue = [];
+		while (obj.length--) {
+			cacheValue[ i ] = deepClone2(obj[ i ]);
+		}
+		return cacheValue;
+	} else if (Object.prototype.toString.call(obj) === "[object Object]") {
+		cacheValue = {};
+		for (var k in obj) {
+			if (obj.hasOwnProperty(k)) {
+				cacheValue[ k ] = deepClone2(obj[ k ]);
 			}
-        }
-        return cacheValue;
-    }else{
-        return obj;
-    }
+		}
+		return cacheValue;
+	} else {
+		return obj;
+	}
 }
 
 // 测试用例：
 var srcObj = {
-    a: 1,
-    b: {
-        b1: ["hello", "hi"],
+	a: 1,
+	b: {
+		b1: [ "hello", "hi" ],
 		b2: "JavaScript",
 		b3: {
-			b4: [1,2,3],
+			b4: [ 1, 2, 3 ],
 			b5: 'test',
-			b6: [4,5,{
-				b7: [6,7,{
-					b8: [8,9,10],
+			b6: [ 4, 5, {
+				b7: [ 6, 7, {
+					b8: [ 8, 9, 10 ],
 					b9: "hi"
-				}],
+				} ],
 				b10: {
 					b11: 2,
-					b12: [11,12,13]
+					b12: [ 11, 12, 13 ]
 				}
-			}]
+			} ]
 		}
-    }
+	}
 };
 var abObj = srcObj;
 var tarObj = deepClone1(srcObj);
 
 srcObj.a = 2;
-srcObj.b.b1[0] = "Hello";
-srcObj.b.b3.b6[2].b10.b12.push(14)
+srcObj.b.b1[ 0 ] = "Hello";
+srcObj.b.b3.b6[ 2 ].b10.b12.push(14)
 
-console.log("2-2", tarObj.a);      
+console.log("2-2", tarObj.a);
 // 1
-console.log("2-2", tarObj.b.b1[0]);    
+console.log("2-2", tarObj.b.b1[ 0 ]);
 // "hello"
-console.log("2-2", tarObj.b.b3.b6[2].b10.b12)
+console.log("2-2", tarObj.b.b3.b6[ 2 ].b10.b12)
 // [11, 12, 13]
 
 
@@ -118,26 +118,26 @@ console.log("2-2", tarObj.b.b3.b6[2].b10.b12)
 // 2-3 学习数组、字符串、数字等相关方法
 // 返回去重后的数组
 function uniqArray1(arr) {
-	if(Array.isArray && Array.isArray(arr)){
+	if (Array.isArray && Array.isArray(arr)) {
 		var cacheArr = [];
-		arr.reduce((ac,cv) => {
-			if(!(cv in ac)){
-				ac[cv] = cv;
+		arr.reduce((ac, cv) => {
+			if (!(cv in ac)) {
+				ac[ cv ] = cv;
 				cacheArr.push(cv);
 				return ac;
-			}else{
+			} else {
 				return ac;
 			}
-		},{})
+		}, {})
 		return cacheArr;
-	}else{
+	} else {
 		throw new Error("array accept");
 	}
 }
 
 // 使用示例
-var a = [1, 3, 5, 7, 5, 3, null, null, undefined, undefined, NaN, NaN, Infinity, Infinity];
-console.log(uniqArray1(a)); 
+var a = [ 1, 3, 5, 7, 5, 3, null, null, undefined, undefined, NaN, NaN, Infinity, Infinity ];
+console.log(uniqArray1(a));
 // [1, 3, 5, 7, null, undefined, NaN, Infinity]
 
 
@@ -145,7 +145,7 @@ console.log(uniqArray1(a));
 
 // 去除字符串头尾的 全角空格、半角空格、Tab等
 function trim(str) {
-    return str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+	return str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
 }
 
 // 使用示例
@@ -157,17 +157,17 @@ console.log(trim(str)); // 'hi!'
 
 // 遍历数组元素以执行 fn 函数，参数为数组索引 和 元素
 function each(arr, fn) {
-    arr.forEach((cv, index) => {
+	arr.forEach((cv, index) => {
 		fn(cv, index);
 	})
 }
 
 // 使用示例
-var arr = ['java', 'c', 'php', 'html'];
+var arr = [ 'java', 'c', 'php', 'html' ];
 function output(item, index) {
-    console.log(index + ': ' + item)
+	console.log(index + ': ' + item)
 }
-each(arr, output);  
+each(arr, output);
 // 0: java, 1: c, 2: php, 3: html
 
 
@@ -176,8 +176,8 @@ each(arr, output);
 // 获取一个对象里面第一层元素的数量，返回一个整数
 function getObjectLength(obj) {
 	var attrNum = 0;
-	for(var key in obj){
-		if(obj.hasOwnProperty(key)){
+	for (var key in obj) {
+		if (obj.hasOwnProperty(key)) {
 			attrNum++;
 		}
 	}
@@ -186,14 +186,14 @@ function getObjectLength(obj) {
 
 // 使用示例
 var obj = {
-    a: 1,
-    b: 2,
-    c: {
-        c1: 3,
-        c2: 4
-    }
+	a: 1,
+	b: 2,
+	c: {
+		c1: 3,
+		c2: 4
+	}
 };
-console.log(getObjectLength(obj)); 
+console.log(getObjectLength(obj));
 // 3
 
 
@@ -202,17 +202,17 @@ console.log(getObjectLength(obj));
 // 2-4 学习正则表达式
 // 判断是否为邮箱地址
 function isEmail(emailStr) {
-    // your implement
+	// your implement
 }
 
 // 判断是否为手机号
 function isMobilePhone(phone) {
-    // your implement
+	// your implement
 }
 
 // 去除头尾空格
-function trimm(str){
-    return str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+function trimm(str) {
+	return str.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
 }
 
 
@@ -237,7 +237,7 @@ function removeClass(element, oldClassName) {
 
 // 判断 siblingNode 和 element 是否为同一个父元素下的同一级的元素，返回 boolean 值 
 function isSiblingNode(element, siblingNode) {
-    return element.parentElement == siblingNode.parentElement;
+	return element.parentElement == siblingNode.parentElement;
 }
 
 // 获取element相对于浏览器窗口的位置，返回一个对象{x, y}
@@ -251,30 +251,30 @@ function getPosition(element) {
 
 // 实现一个简单的 Query 不直接使用document.querySelector
 function $(selector) {
-    return document.querySelector(selector);
+	return document.querySelector(selector);
 }
 
 // 可以通过 id 获取 DOM 对象，通过 # 标示，例如
-$("#adom"); 
+$("#adom");
 // 返回 id 为 adom 的 DOM 对象
 
 // 可以通过 tagName 获取 DOM 对象，例如
-$("a"); 
+$("a");
 // 返回第一个 <a>对象
 
 // 可以通过样式名称获取DOM对象，例如
-$(".classa"); 
+$(".classa");
 // 返回第一个样式定义包含 classa 的对象
 
 // 可以通过 attribute 匹配获取 DOM 对象，例如
-$("[data-log]"); 
+$("[data-log]");
 // 返回第一个包含属性 data-log 的对象
 
-$("[data-time=2015]"); 
+$("[data-time=2015]");
 // 返回第一个包含属性 data-time 且值为 2015 的对象
 
 // 可以通过简单的组合提高查询便利性，例如
-$("#adom .classa"); 
+$("#adom .classa");
 // 返回 id 为 adom 的DOM所包含的所有子节点中，第一个样式定义包含 classa 的对象
 
 
@@ -292,7 +292,7 @@ $("#adom .classa");
 // 继续封装 Jquery
 // 给一个 element 绑定一个针对 event 事件的响应，响应函数为 listener
 function addEvent(element, event, listener) {
-    element.addEventListener(event, listener);
+	element.addEventListener(event, listener);
 }
 
 // 例如：
@@ -321,18 +321,18 @@ function addEnterEvent(element, listener) {
 // addClickEvent(element, listener) -> $.click(element, listener);
 // addEnterEvent(element, listener) -> $.enter(element, listener);
 
-(function(){
+(function () {
 	var $ = {
-		on: function(ele, event, listener){
+		on: function (ele, event, listener) {
 
 		},
-		un: function(ele, event, listener){
+		un: function (ele, event, listener) {
 
 		},
-		click: function(ele, listener){
+		click: function (ele, listener) {
 
 		},
-		enter: function(ele, listener){
+		enter: function (ele, listener) {
 
 		}
 	}
@@ -347,17 +347,29 @@ function addEnterEvent(element, listener) {
 
 // 判断是否为IE浏览器，返回-1或者版本号
 function isIE() {
-    // your implement
+	// your implement
 }
 
 // 设置cookie
 function setCookie(cookieName, cookieValue, expiredays) {
-    // your implement
+	const d = new Date();
+	d.setTime(d.getTime() + (expiredays * 24 * 60 * 60 * 1000));
+	// 设置 cookie 的过期时间 expires
+	const expires = "expires=" + d.toGMTString();
+	document.cookie = cookieName + "=" + cookieValue + "; " + expires;
 }
 
 // 获取cookie值
 function getCookie(cookieName) {
-    // your implement
+	var name = cookieName + "=";
+	var cache = document.cookie.split(';');
+	for(let i=0; i < cache.length; i++){
+	  var cv = cache[i].trim();
+	  if (cv.indexOf(name) === 0) {
+		return cv.substring(name.length, cv.length);
+	  }
+	}
+	return "";
 }
 
 
@@ -375,21 +387,21 @@ function getCookie(cookieName) {
 // onsuccess: 成功时的调用函数
 // onfail: 失败时的调用函数
 function ajax(url, options) {
-    // your implement
+	// your implement
 }
 
 // 使用示例：
 ajax(
-    'http://localhost:8080/server/ajaxtest', 
-    {
-        data: {
-            name: 'simon',
-            password: '123456'
-        },
-        onsuccess: function (responseText, xhr) {
-            console.log(responseText);
-        }
-    }
+	'http://localhost:8080/server/ajaxtest',
+	{
+		data: {
+			name: 'simon',
+			password: '123456'
+		},
+		onsuccess: function (responseText, xhr) {
+			console.log(responseText);
+		}
+	}
 );
 
 // 
