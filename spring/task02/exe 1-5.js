3485cbb77285bc99c1bc1cff2cee38da3a157f93
rjwx60 <614054722@qq.com>
2019-05-07 21:37:56 +0800

    task2

--- spring/task02/exe 1-5.js
+++ spring/task02/exe 1-5.js
@@ -4,64 +4,70 @@
 // 当点击按钮时，把用户输入的兴趣爱好，按照上面所说的分隔符分开后保存到一个数组，过滤掉空的、重复的爱好，在按钮下方创建一个段落显示处理后的爱好。
 // 思路: input querySelector split trim 去重 显示
 
-var Btn1 = $('.Btn1');
+var Btn1 = $(".Btn1");
 
 addClickEvent(Btn1, () => {
-  var Inp1 = $('.Inp1');
-  $('.showPlace').innerHTML = Array.from(new Set(Inp1.value.split(','))).join(' ');
-})
+  var Inp1 = $(".Inp1");
+  $(".showPlace").innerHTML = Array.from(new Set(Inp1.value.split(","))).join(
+    " "
+  );
+});
 
 // 阶段2:
 // 单行变成多行输入框，一个按钮，输入框中用来输入用户的兴趣爱好，允许用户用换行、空格（全角/半角）、逗号（全角/半角）、顿号、分号来作为不同爱好的分隔。
 // 当点击按钮时的行为同上
 // 思路：textarea querySelector replace split trim 去重 显示
 
-var Btn1 = $('.Btn2');
+var Btn1 = $(".Btn2");
 
 addClickEvent(Btn1, () => {
-  var Inp2 = $('.Inp2');
-  $('.showPlace').innerHTML = Array.from(new Set(Inp2.value.split(/,|\n|，|;|；|\s/g).filter(cv => cv))).join(' ');
-})
+  var Inp2 = $(".Inp2");
+  $(".showPlace").innerHTML = Array.from(
+    new Set(Inp2.value.split(/,|\n|，|;|；|\s/g).filter(cv => cv))
+  ).join(" ");
+});
 
-// 阶段3: 
+// 阶段3:
 // 用户输入的爱好数量不能超过10个，也不能什么都不输入。当发生异常时，在按钮上方显示一段红色的错误提示文字，并且不继续执行后面的行为；当输入正确时，提示文字消失。
 // 同时，当点击按钮时，不再是输出到一个段落，而是每一个爱好输出成为一个 checkbox ，爱好内容作为 checkbox 的 label。
 // 思路：键盘输入事件 内容为空不算+分隔号之间为空不算 类控制提示文字显示 组成的
 
-var Btn3 = $('.Btn3'),
-    Btn3Info = $('.Input3_info'),
-    Inp3 = $('.Inp3'),
-    Exe1 = $('.Exe1');
+var Btn3 = $(".Btn3"),
+  Btn3Info = $(".Input3_info"),
+  Inp3 = $(".Inp3"),
+  Exe1 = $(".Exe1");
 
 const dubHobbyArray = value => {
-  return Array.from(new Set(value.split(/,|\n|，|;|；|\s/g).filter(cv => cv)))
-}
+  return Array.from(new Set(value.split(/,|\n|，|;|；|\s/g).filter(cv => cv)));
+};
 
-addEvent(Inp3, 'keyup', (event) => {
-  var infoText = '',
-      result = dubHobbyArray(event.target.value);
-      infoText = result.length ? result.length <= 10 ? '' : '输入个数不能超过10个' : '输入内容不能为空';
-      Btn3Info.innerHTML = infoText;
-})
+addEvent(Inp3, "keyup", event => {
+  var infoText = "",
+    result = dubHobbyArray(event.target.value);
+  infoText = result.length
+    ? result.length <= 10
+      ? ""
+      : "输入个数不能超过10个"
+    : "输入内容不能为空";
+  Btn3Info.innerHTML = infoText;
+});
 
 addClickEvent(Btn3, () => {
-  var div = document.createElement('div');
+  var div = document.createElement("div");
   dubHobbyArray(Inp3.value).forEach((cv, index) => {
-    var label = document.createElement('label'),
-        input = document.createElement('input'),
-        p = document.createElement('p');
-    label.setAttribute('for', `hobby-${index+1}`);
+    var label = document.createElement("label"),
+      input = document.createElement("input"),
+      p = document.createElement("p");
+    label.setAttribute("for", `hobby-${index + 1}`);
     label.innerHTML = cv;
-    input.setAttribute('id', `hobby-${index+1}`);
-    input.setAttribute('type', 'checkbox');
+    input.setAttribute("id", `hobby-${index + 1}`);
+    input.setAttribute("type", "checkbox");
     p.appendChild(label);
     p.appendChild(input);
     div.appendChild(p);
   });
   Exe1.appendChild(div);
-})
-
-
+});
 
 // 练习2:
 // 实现一个倒计时功能。
@@ -72,49 +78,52 @@ addClickEvent(Btn3, () => {
 // 每一秒钟更新倒计时上显示的数
 // 如果时差为0，则倒计时停止
 
-var Btn4 = $('.Btn4');
+var Btn4 = $(".Btn4");
+var timer, minValue;
 
 addClickEvent(Btn4, () => {
-  var Inp4 = $('.Inp4');
-  var initTime = new Date(`${Inp4.value.split('-').map(cv => parseInt(cv)).join('-')}`).getTime();
-
+  var Inp4 = $(".Inp4");
+  var initTime = new Date(
+    `${Inp4.value
+      .split("-")
+      .map(cv => parseInt(cv))
+      .join("-")}`
+  ).getTime();
   // 初始显示
   timeOperate(initTime - Date.now());
   // 后续显示
   refreshTime(initTime);
-})
+});
 
-function refreshTime(futureTime){
-  let minValue;
-
+function refreshTime(futureTime) {
   clearInterval(timer);
-  
-  var timer = setInterval(() => {
+
+  timer = setInterval(() => {
     minValue = futureTime - Date.now();
-    if(minValue <= 0){
-      $('.showPlace').innerHTML = '';
+    if (minValue <= 0) {
       clearInterval(timer);
-    }else{
+      $(".showPlace").innerHTML = "";
+    } else {
       timeOperate(minValue);
     }
   }, 1000);
 }
-function timeOperate(minValue){
-  let day = parseInt(minValue / 1000 / 60 / 60 / 24 ),
-      hour = minValue / 1000 / 60 / 60,
-      minute = minValue / 1000 / 60;
-      second = minValue  / 1000;
 
-  let obj ={
+function timeOperate(minValue) {
+  let day = parseInt(minValue / 1000 / 60 / 60 / 24),
+    hour = minValue / 1000 / 60 / 60,
+    minute = minValue / 1000 / 60;
+  second = minValue / 1000;
+  let obj = {
     day: day,
     hour: parseInt(hour - day * 24),
     minute: parseInt(minute - parseInt(hour) * 60),
-    second: parseInt(second - parseInt(minute) * 60),
+    second: parseInt(second - parseInt(minute) * 60)
   };
-
-  $('.showPlace').innerHTML = `${obj.day} ${obj.hour} ${obj.minute} ${obj.second}`;
+  $(".showPlace").innerHTML = `${obj.day} ${obj.hour} ${obj.minute} ${
+    obj.second
+  }`;
 }
-
 
 // 练习3:
 // 实现一个轮播图的功能。
@@ -123,32 +132,33 @@ function timeOperate(minValue){
 // 图片切换的动画要流畅
 // 在轮播图下方自动生成对应图片的小点，点击小点，轮播图自动动画切换到对应的图片
 
-window.onload = function(){
-  var Exe3Run = $('.Exe3_Run'), tag = 0;
+window.onload = function() {
+  var Exe3Run = $(".Exe3_Run"),
+    tag = 0;
 
-  function AnimationRun(speed, direction){
-    let timer = null, direct = direction ? '-' : '';
+  function AnimationRun(speed, direction) {
+    let timer = null,
+      direct = direction ? "-" : "";
     // console.log(window.getComputedStyle(Exe3Run).width);
     clearInterval(timer);
     timer = setInterval(() => {
       tag += speed;
-      Exe3Run.style.setProperty('left', `${direct}${tag}px`);
-      if(tag === 2000){
-        
-        Exe3Run.style.setProperty('left', `0px`);
+      Exe3Run.style.setProperty("left", `${direct}${tag}px`);
+      if (tag === 2000) {
+        Exe3Run.style.setProperty("left", `0px`);
 
         tag = 0;
       }
-      if(tag % 400 === 0){
+      if (tag % 400 === 0) {
         clearInterval(timer);
-        setTimeout(function(){
+        setTimeout(function() {
           AnimationRun();
-        },1000)
+        }, 1000);
       }
     }, 10);
   }
   AnimationRun(5, false);
-}
+};
 
 // 练习4：
 // 实现一个类似百度搜索框的输入提示的功能，使用Ajax来获取提示数据。
@@ -156,8 +166,6 @@ window.onload = function(){
 // 允许使用鼠标点击选中提示栏中的某个选项
 // 允许使用键盘上下键来选中提示栏中的某个选项，回车确认选中
 // 选中后，提示内容变更到输入框中
-
-
 
 // 练习5：
 // 实现一个可拖拽交互的界面
